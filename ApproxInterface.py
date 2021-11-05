ab93962568d72096092f58ea80a872dbe1373b1e
Mason Medina <masonmedina29@gmail.com>
2021-11-04 18:23:52 -0600

    Markov Working Gibbs getting there

--- ApproxInterface.py
+++ ApproxInterface.py
@@ -1,59 +1,58 @@
 import random as rand
-from BayesNet import BayesNet
+
 
 class ApproxInterface:
     def __init__(self):
         pass
 
-    def normalize(self, node):
-        return [x / sum(node) for x in data]
-
-    def markov_blanket(self, X, e, bn):
+    def get_markov_blanket(self, X, bn):
 
         markov_list = []
-        Node = BayesNet.getNode(bn, X)
+        node = bn.getNode(X)
 
-        for child in Node.children:
+        # get the children and children's parents of selected node
+
+        for child in node.children:
             markov_list.append(child)
-            for child_parent in Node.parent:
-                markov_list.append(parent)
+            for child_parent in node.parent:
+                markov_list.append(child_parent)
 
-        for parent in Node.parent:
+        # get the parents of selected node
+
+        for parent in node.parent:
             markov_list.append(parent)
 
-        print(markov_list)
+        list(set(markov_list))
+
+        '''
+        Test Statement
+        
+        for i in markov_list:
+            node = bn.getNode(i)
+            print(node.name)
+        
+        '''
+
 
         return markov_list
 
-
-
-        #for child in Node.children:
-
-
-        pass
-
-    def gibbsSampling(self, X, e, bn, n=1000):
+    def gibbsSampling(self, X, evidence, bn, N=1000):
 
         count = []
-        Z = [var for var in bn.nodes if var not in e]
-        x = e
+        Z = [var for var in bn.nodes if var not in evidence]
+        x = evidence
+
+        # random choice each domain of a node then set it
+
+        for node in bn.nodes:
+                node.domain = rand.choice(node.domain)
+                print(node.name)
+                print(node.domain)
+
+        for j in range(N):
+            for node in bn.nodes:
+                pass
+
 
 
         pass
-        """
-      
-
-        for Zi in Z:
-            x[Zi] = rand.choice(bn.variables(Zi))
-        for j in range(N):
-            for Zi in Z:
-                x[Zi] = markov_blanket(Zi, x, bn)
-                count[x[X]] += 1
-        """
-
-
-
-
-
-
-
